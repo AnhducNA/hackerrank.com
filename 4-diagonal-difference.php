6918b1ff04e943916b524f647331f3506acf8b41
AnhducNA <Anhduc05022001>
2023-01-03 15:31:43 +0700

    4-diagonal-difference.php

--- 4-diagonal-difference.php
+++ 4-diagonal-difference.php
@@ -1,22 +1,25 @@
 <?php
 
-function diagonalDifference($arr) {
-    $d_ar = [];
-    $main_len = count($arr);
-
-    foreach($arr as $key => $ar) {
-        $sub_len = count($ar);
-        if($main_len != $sub_len) {
-            throw new Exception('Array index count mismatched. Hence this is not square array.');
+function diagonalDifference($arr)
+{
+    $countArr = count($arr);
+    $principal_diagonal_sum = 0;
+    $second_diagonal_sum = 0;
+    foreach ($arr as $key => $sub_arr) {
+        $sub_countArr = count($sub_arr);
+        if($countArr != $sub_countArr){
+            throw new Exception('Array index count mismatched. This is not square array.');
+        }else{
+            $principal_diagonal_sum += $sub_arr[$key];
+            $second_diagonal_sum += $sub_arr[$countArr-$key-1];
         }
-        $d_ar[0][] = $ar[$key];
-        $d_ar[1][] = $ar[($main_len-$key-1)];
     }
-    return abs((array_sum($d_ar[0]) - array_sum($d_ar[1])));
+    return abs($principal_diagonal_sum - $second_diagonal_sum);
 }
+
 $arr = [
-    [1,2,3],
-    [4,5,6],
-    [9,8,9]
+    [1, 2, 3],
+    [4, 5, 6],
+    [9, 8, 9]
 ];
 echo diagonalDifference($arr);
